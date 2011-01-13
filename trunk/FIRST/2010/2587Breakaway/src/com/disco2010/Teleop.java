63e23e4ce4c7b2b3c4db43027b869dbd29801d6d
nchen99 <nchen99@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-01-13 03:02:36 +0000

    added comments, moved lines around and grouped commented-out lines at the bottom

--- trunk/FIRST/2010/2587Breakaway/src/com/disco2010/Teleop.java
+++ trunk/FIRST/2010/2587Breakaway/src/com/disco2010/Teleop.java
@@ -2,8 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.disco2010;
+
 import edu.wpi.first.wpilibj.*;
 
 /**
@@ -11,151 +11,139 @@ import edu.wpi.first.wpilibj.*;
  * @author pavchag
  */
 public class Teleop {
+
     double ballIn;
     boolean kicking = false;
     boolean loading;
     Timer loadTime;
-    //Latch latch = new Latch(true);
-    public void start()
-    {
 
-        if(HW.driveStick2.getRawButton(10))
+    public void start() {
+
+        if (HW.driveStick2.getRawButton(10)) {
             HW.kickerMotor.set(-0.6);
-        else if(HW.driveStick2.getRawButton(11))
+        } else if (HW.driveStick2.getRawButton(11)) {
             HW.kickerMotor.set(1.0);
+        }
 
         Watchdog.getInstance().setExpiration(1000);
 
         HW.drive.holonomicDrive(HW.driveStick1.getMagnitude(), HW.driveStick1.getDirectionDegrees(), HW.driveStick2.getX());
-        ///HW.tracker.track();
         HW.rightDribbler.set(-HW.kickhandle.getAxis(Joystick.AxisType.kY));
         HW.leftDribbler.set(HW.kickhandle.getAxis(Joystick.AxisType.kY));
 
-
-        
-        /*if(HW.kickhandle.getRawButton(11))
-        {
-            HW.camTilt.set(DiscoUtils.rawToPosition((double) HW.camTilt.getRaw()) + 0.05);
-        }
-        else if(HW.kickhandle.getRawButton(10))
-        {
-            HW.camTilt.set(DiscoUtils.rawToPosition((double) HW.camTilt.getRaw()) - 0.05);
-        }
-
-        if(HW.kickhandle.getRawButton(8))
-        {
-            HW.camPan.set(DiscoUtils.rawToPosition((double) HW.camPan.getRaw()) + 0.05);
-        }
-        else if(HW.kickhandle.getRawButton(9))
-        {
-            HW.camPan.set(DiscoUtils.rawToPosition((double) HW.camPan.getRaw()) - 0.05);
-        }*/
-
-        if (HW.kickhandle.getTrigger() ||
-            HW.driveStick2.getTrigger() &&
-            HW.kickSwitch.get() &&
-            !kicking)
-        {
+        if (HW.kickhandle.getTrigger()
+                || HW.driveStick2.getTrigger()
+                && HW.kickSwitch.get()
+                && !kicking) {
             HW.kicker.kick(4);
             loading = false;
             kicking = true;
-        }
-        else if (HW.kickhandle.getRawButton(5) == true ||
-                 HW.driveStick2.getRawButton(5) &&
-                 HW.kickSwitch.get() == true &&
-                 !kicking)
-        {
+        } else if (HW.kickhandle.getRawButton(5) == true
+                || HW.driveStick2.getRawButton(5)
+                && HW.kickSwitch.get() == true
+                && !kicking) {
             HW.kicker.kick(3);
             loading = false;
             kicking = true;
-        }
-        else if (HW.kickhandle.getRawButton(3) == true ||
-                 HW.driveStick2.getRawButton(3) &&
-                 HW.kickSwitch.get() == true &&
-                 !kicking)
-        {
+        } else if (HW.kickhandle.getRawButton(3) == true
+                || HW.driveStick2.getRawButton(3)
+                && HW.kickSwitch.get() == true
+                && !kicking) {
             HW.kicker.kick(2);
             loading = false;
             kicking = true;
-        }
-        else if(HW.kickhandle.getRawButton(4) == true ||
-                HW.driveStick2.getRawButton(4) &&
-                HW.kickSwitch.get() == true &&
-                !kicking)
-        {
+        } else if (HW.kickhandle.getRawButton(4) == true
+                || HW.driveStick2.getRawButton(4)
+                && HW.kickSwitch.get() == true
+                && !kicking) {
             HW.kicker.kick(1);
             loading = false;
             kicking = true;
-        }
-        else if (!HW.kickhandle.getTrigger() &&
-                 !HW.kickhandle.getRawButton(4) &&
-                 !HW.kickhandle.getRawButton(3) &&
-                 !HW.kickhandle.getRawButton(5) &&
-                 !HW.driveStick2.getTrigger() &&
-                 !HW.driveStick2.getRawButton(4) &&
-                 !HW.driveStick2.getRawButton(3) &&
-                 !HW.driveStick2.getRawButton(5) &&
-                  HW.loadSwitch.get())
-        {
+        } else if (!HW.kickhandle.getTrigger()
+                && !HW.kickhandle.getRawButton(4)
+                && !HW.kickhandle.getRawButton(3)
+                && !HW.kickhandle.getRawButton(5)
+                && !HW.driveStick2.getTrigger()
+                && !HW.driveStick2.getRawButton(4)
+                && !HW.driveStick2.getRawButton(3)
+                && !HW.driveStick2.getRawButton(5)
+                && HW.loadSwitch.get()) {
             HW.kicker.load();
             loading = true;
             kicking = false;
         }
-        if(!HW.loadSwitch.get())
-        {
+        if (!HW.loadSwitch.get()) {
             HW.kicker.kill();
             loading = false;
             kicking = false;
         }
 
-        if(HW.kickhandle.getRawButton(2))
-        {
+        if (HW.kickhandle.getRawButton(2)) {
             HW.camPan.set(0.5);
             HW.camTilt.set(0.5);
         }
 
-        if(HW.kickhandle.getRawButton(6) && HW.barUpSwitch.get())
+        if (HW.kickhandle.getRawButton(6) && HW.barUpSwitch.get()) {
             HW.kicker.moveBar(true);
-        else if(HW.kickhandle.getRawButton(7) && HW.barDownSwitch.get())
+        } else if (HW.kickhandle.getRawButton(7) && HW.barDownSwitch.get()) {
             HW.kicker.moveBar(false);
-            else
-                HW.kickerBar.set(0.0);
+        } else {
+            HW.kickerBar.set(0.0);
+        }
+
+        DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "Gyro: " + HW.gyro.getAngle());
+        DriverStationLCD.getInstance().updateLCD();
+
+        if (HW.driveStick2.getRawButton(4)) {
+            HW.rightEncoder.reset();
+        }
         
+        //Camera Control for Subsystem Operator
+        if (HW.kickhandle.getRawButton(11)) {
+            HW.camTilt.set(DiscoUtils.rawToPosition((double) HW.camTilt.getRaw()) + 0.05);
+        } else if (HW.kickhandle.getRawButton(10)) {
+            HW.camTilt.set(DiscoUtils.rawToPosition((double) HW.camTilt.getRaw()) - 0.05);
+        }
+
+        if (HW.kickhandle.getRawButton(8)) {
+            HW.camPan.set(DiscoUtils.rawToPosition((double) HW.camPan.getRaw()) + 0.05);
+        } else if (HW.kickhandle.getRawButton(9)) {
+            HW.camPan.set(DiscoUtils.rawToPosition((double) HW.camPan.getRaw()) - 0.05);
+        }
+
+        /* DEBUG PRINTING
+        //DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser4, 1, "I/O 8 = " + HW.autonMode.get());
+        //DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "kickSwitch " + kickSwitch);
+        //DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "REncDist: " + HW.rightEncoder.getDistance());
+        //DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser6, 1, "LEncDist: " + HW.leftEncoder.getDistance());
         //String windowUp;
         //String windowDown;
         //String kickSwitch;
         //String loadSwitch;
 
         /*if(!HW.barUpSwitch.get())
-            windowUp = "Pressed";
+        windowUp = "Pressed";
         else windowUp = "Nope";
 
         if(!HW.barDownSwitch.get())
-            windowDown = "Pressed";
+        windowDown = "Pressed";
         else windowDown = "Nope";*/
 
         /*if(!HW.kickSwitch.get())
-            kickSwitch = "Pressed";
+        kickSwitch = "Pressed";
         else kickSwitch = "Nope";*/
 
         /*if(!HW.loadSwitch.get())
-            loadSwitch = "Pressed";
-        else loadSwitch = "Nope";*/
-        DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser4, 1, "I/O 8 = " + HW.autonMode.get());
-        // DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "kickSwitch " + kickSwitch);
-        //DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "REncDist: " + HW.rightEncoder.getDistance());
-        //DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser6, 1, "LEncDist: " + HW.leftEncoder.getDistance());
-        DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "Gyro: " + HW.gyro.getAngle());
-        DriverStationLCD.getInstance().updateLCD();
-
-        /*System.out.println("TstEncDst: " + HW.leftEncoder.getDistance());
+        loadSwitch = "Pressed";
+        else loadSwitch = "Nope";
+        System.out.println("TstEncDst: " + HW.leftEncoder.getDistance());
         System.out.println("Distance: " + HW.rightEncoder.getDistance());
         System.out.println("Raw: " + HW.rightEncoder.getRaw());
-        System.out.println("Get: " + HW.rightEncoder.get());*/
+        System.out.println("Get: " + HW.rightEncoder.get());
+         */
 
-        if(HW.driveStick2.getRawButton(4))
-            HW.rightEncoder.reset();
-
+        /* The Tracking lines
+        //HW.tracker.track();
+         */
     }
-
 }
