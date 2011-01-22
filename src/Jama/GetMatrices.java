9a06aef3af3c2b3e422f399cc275bb610e4b1b03
pavchag@gmail.com <pavchag@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-01-22 03:45:28 +0000

    Updated Matrix code for RobotVelocity

---
+++ src/Jama/GetMatrices.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Jama;
+
+import Jama.*;
+import java.util.*;
+/**
+ *
+ * @author pavchag
+ */
+public class GetMatrices
+{
+    double theta = Math.PI/2;
+
+    double[][] wheelVel = new double[3][0];
+    double[][] velCoupl = new double[3][4];
+    double[][] wheelForce = new double[3][0];
+    double[][] forceCoupl = new double[2][3];
+    
+    public Matrix getWheelVelMat(double v1, double v2, double v3, double v4)
+    {
+        wheelVel[0][0] = v1;
+        wheelVel[0][1] = v2;
+        wheelVel[0][2] = v3;
+        wheelVel[0][3] = v4;
+        Matrix wheelVelMat = new Matrix(wheelVel);
+        return wheelVelMat;
+    }
+    public Matrix getVelCouplMat()
+    {
+        velCoupl[0][0] = -Math.sin(Math.PI/2);
+        velCoupl[0][1] = Math.cos(Math.PI/2);
+        velCoupl[0][2] = 1;
+
+        velCoupl[1][0] = -Math.sin(Math.PI/2);
+        velCoupl[1][1] = Math.cos(Math.PI/2);
+        velCoupl[1][2] = 1;
+
+        velCoupl[2][0] = -Math.sin(Math.PI/2);
+        velCoupl[2][1] = Math.cos(Math.PI/2);
+        velCoupl[2][2] = 1;
+
+        velCoupl[3][0] = -Math.sin(Math.PI/2);
+        velCoupl[3][1] = Math.cos(Math.PI/2);
+        velCoupl[3][2] = 1;
+        Matrix velCouplMat = new Matrix(velCoupl);
+        return velCouplMat;
+    }
+
+    // This code is for the acceleration of the robot.
+    public Matrix getWheelForceMat(double f1, double f2, double f3, double f4)
+    {
+        wheelForce[0][0] = f1;
+        wheelForce[0][1] = f2;
+        wheelForce[0][2] = f3;
+        wheelForce[0][3] = f4;
+        Matrix wheelForceMat = new Matrix(wheelForce);
+        return wheelForceMat;
+    }
+    public Matrix getForceCouplMat(double alpha)
+    {
+        forceCoupl[0][0] = -Math.sin(Math.PI/2);
+        forceCoupl[0][1] = -Math.sin(Math.PI/2);
+        forceCoupl[0][2] = -Math.sin(Math.PI/2);
+        forceCoupl[0][3] = -Math.sin(Math.PI/2);
+
+        forceCoupl[1][0] = Math.cos(Math.PI/2);
+        forceCoupl[1][1] = Math.cos(Math.PI/2);
+        forceCoupl[1][2] = Math.cos(Math.PI/2);
+        forceCoupl[1][3] = Math.cos(Math.PI/2);
+
+        forceCoupl[2][0] = 1/alpha;
+        forceCoupl[2][1] = 1/alpha;
+        forceCoupl[2][2] = 1/alpha;
+        forceCoupl[2][3] = 1/alpha;
+
+        Matrix forceCouplMat = new Matrix(forceCoupl);
+        return forceCouplMat;
+    }
+}
