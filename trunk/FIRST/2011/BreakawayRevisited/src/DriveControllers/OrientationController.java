f67f40562281132f5ee5b853ae218a5b67401efc
nchen99@gmail.com <nchen99@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-01-27 03:49:22 +0000

    OrientationController still unfinished

--- trunk/FIRST/2011/BreakawayRevisited/src/DriveControllers/OrientationController.java
+++ trunk/FIRST/2011/BreakawayRevisited/src/DriveControllers/OrientationController.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package DriveControllers;
 
 import Utils.*;
@@ -14,23 +13,19 @@ import edu.wpi.first.wpilibj.*;
  */
 public class OrientationController {
 
-    final double ki = 5;
+    final double ki = 20.0;
     double iterm = 0.0;
     final double itermMax = 30;
-
+    final double maxError = 1.0;
     double turnVelocity = 20.0;
-
     double originalHeading;
-    double goalHeading;
+    final double goalHeading = 0.0;
     double oldTime;
-
     Gyro gyro;  //positive angle change is clockwise turn
     VelocityController leftMotor;
     VelocityController rightMotor;
-
     double leftGoalVelocity;
     double rightGoalVelocity;
-
     double leftVelocityOutput;
     double rightVelocityOutput;
 
@@ -43,47 +38,59 @@ public class OrientationController {
 
     public void initialize() {
         oldTime = Timer.getFPGATimestamp();
-        goalHeading = gyro.getAngle();
+        //goalHeading = gyro.getAngle();
         iterm = 0.0;
     }
 
     /*public void setTurn(double degrees) {
-        originalHeading = gyro.getAngle();
-        goalHeading = originalHeading + degrees;
-        leftVelocity = turnVelocity;
-        rightVelocity = turnVelocity;
+    originalHeading = gyro.getAngle();
+    goalHeading = originalHeading + degrees;
+    leftVelocity = turnVelocity;
+    rightVelocity = turnVelocity;
     }*/
-
     private void adjustVelocity() {
         double timeDiff = (Timer.getFPGATimestamp() - oldTime);
 
-        double error = gyro.getAngle() - goalHeading;
+        double error = goalHeading + gyro.getAngle();
         //DiscoUtils.debugPrintln("\n\nHeading error: " + error);
+
+        if(error < maxError)
+            iterm = 0.0;
 
         if (Math.abs(iterm) < itermMax) {
             iterm += error * timeDiff;
             DiscoUtils.debugPrintln("\n\nHeading iterm: " + iterm);
+            DiscoUtils.debugPrintln("Gyro: " + gyro.getAngle());
         }
-        leftVelocityOutput = leftGoalVelocity + iterm * ki;
-        rightVelocityOutput = rightGoalVelocity - iterm * ki;
+        leftVelocityOutput = limit(leftGoalVelocity - iterm * ki);
+        rightVelocityOutput = limit(rightGoalVelocity + iterm * ki);
         DiscoUtils.debugPrintln("leftOutput: " + leftVelocityOutput);
         DiscoUtils.debugPrintln("rightOutput: " + rightVelocityOutput);
         DiscoUtils.debugPrintln("leftVelocity: " + leftMotor.getVelocity());
         DiscoUtils.debugPrintln("rightVelocity: " + rightMotor.getVelocity());
-        DiscoUtils.debugPrintln("leftGoal: " + leftMotor.getGoalVelocity());
-        DiscoUtils.debugPrintln("rightGoal: " + rightMotor.getGoalVelocity());
+        DiscoUtils.debugPrintln("OLDleftGoal: " + leftMotor.getGoalVelocity());
+        DiscoUtils.debugPrintln("OLDrightGoal: " + rightMotor.getGoalVelocity());
+    }
+
+    private double limit(double value) {
+        if (value >= 100.0) {
+            return 100.0;
+        } else if (value <= -100.0) {
+            return -100.0;
+        } else {
+            return value;
+        }
     }
 
     public void controller() {
         if ((Timer.getFPGATimestamp() - oldTime) > 0.05) {
             adjustVelocity();
+            oldTime = Timer.getFPGATimestamp();
             leftMotor.setGoalVelocity(leftVelocityOutput);
             rightMotor.setGoalVelocity(rightVelocityOutput);
             rightMotor.controller();
             leftMotor.controller();
-            oldTime = Timer.getFPGATimestamp();
-        }
-        else {
+        } else {
             rightMotor.controller();
             leftMotor.controller();
         }
@@ -101,9 +108,8 @@ public class OrientationController {
     public double getLeftOutput() {
         return 0.0;
     }
+
     public double getRightOutput() {
         return 0.0;
     }
-
-
 }
