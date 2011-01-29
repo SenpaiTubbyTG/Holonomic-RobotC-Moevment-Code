9e8946c5b6d0ab22c02fd21007ccb49e58eb3a0b
nchen99 <nchen99@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-01-29 21:08:50 +0000

    RobotVelAccel and GetMatrices merged into static class RobotMotion in main package; added Utils

--- branches/RobotVelocity/src/main/Main.java
+++ branches/RobotVelocity/src/main/Main.java
@@ -4,12 +4,10 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package main;
 
-
-import edu.wpi.first.wpilibj.IterativeRobot;
-import Jama.*;
+import Utils.*;
+import edu.wpi.first.wpilibj.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,26 +17,60 @@ import Jama.*;
  * directory.
  */
 public class Main extends IterativeRobot {
+    boolean stopped = true;
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
+        HW.drive.setInvertedMotor(DiscoDrive.kLeftMotor, true); //Invert Left
+        HW.drive.setInvertedMotor(DiscoDrive.kRightMotor, true); //Invert Right
+    }
 
+    /**
+     * This function is called once every time the robot is disabled
+     */
+    public void disabledInit() {
+        HW.frontEncoder.reset();
+        HW.frontEncoder.stop();
+        HW.rearEncoder.reset();
+        HW.rearEncoder.stop();
+    }
+
+    /**
+     * Periodic code for disabled mode should go here.
+     */
+    public void disabledPeriodic() {
+    }
+
+    /**
+     * This function is called once before Autonomous
+     */
+    public void autonomousInit() {
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
     }
 
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic()
-    {
+    public void teleopInit() {
+        HW.frontEncoder.setDistancePerPulse((8 * Math.PI) / 47 / 3);
+        HW.frontEncoder.start();
+        HW.frontEncoder.reset();
+        HW.rearEncoder.setDistancePerPulse((8 * Math.PI) / 47 / 3);
+        HW.rearEncoder.start();
+        HW.rearEncoder.reset();
     }
-    
+
+    public void teleopPeriodic() {
+        HW.drive.basicDrive(0.0,
+                            0.0,
+                            HW.driveStick1.getY(),
+                            HW.driveStick2.getY());
+    }
 }
