d9009ed6b0d9584a1a37e2a90e216a0e46317bde
nchen99@gmail.com <nchen99@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-02-07 22:30:53 +0000

    added reading capability for double and String variables

--- trunk/FIRST/2011/DiscoMotion/src/Utils/FileIO.java
+++ trunk/FIRST/2011/DiscoMotion/src/Utils/FileIO.java
@@ -10,21 +10,23 @@ import com.sun.squawk.microedition.io.FileConnection;
 public class FileIO {
 
     private DataOutputStream logFile;
+    public DataInputStream inFile;
     private FileConnection connection;
 
     /** -------------------------------------------------------
     @method FileIO Constructor
     @param fileName - file name
-    @purpose constructor - creates a FileIO Utility for writing files (reading to be added)
+    @purpose constructor - creates a FileIO Utility for reading and writing files
     @author Nelson Chen
     @written Jan 27, 2011
     ------------------------------------------------------- */
     public FileIO(String fileName) {
         try {
             connection = (FileConnection) Connector.open("file:///" + fileName,
-                    Connector.WRITE);
+                    Connector.READ_WRITE);
             connection.create();
             logFile = connection.openDataOutputStream();
+            inFile = connection.openDataInputStream();
         } catch (Exception e) {
             DiscoUtils.debugPrintln("File Write Error");
         }
@@ -78,14 +80,45 @@ public class FileIO {
     }
 
     /** -------------------------------------------------------
+    @method readLine
+    @purpose reads and returns a String
+    @author Nelson Chen
+    @written Jan 27, 2011
+    ------------------------------------------------------- */
+    public String readUTF() {
+        try {
+            return inFile.readUTF();
+        } catch (Exception e) {
+            DiscoUtils.debugPrintln("File Read Error");
+            return "File Read Error";
+        }
+    }
+
+    /** -------------------------------------------------------
+    @method readDouble
+    @purpose reads and returns a double
+    @author Nelson Chen
+    @written Jan 27, 2011
+    ------------------------------------------------------- */
+    public double readDouble() {
+        try {
+            return inFile.readDouble();
+        } catch (Exception e) {
+            DiscoUtils.debugPrintln("File Read Error");
+            return -999.999;
+        }
+    }
+
+    /** -------------------------------------------------------
     @method close
-    @purpose closes the reader/writer
+    @purpose closes the reader and writer
     @author Nelson Chen
     @written Jan 27, 2011
     ------------------------------------------------------- */
     public void close() {
         try {
             logFile.close();
+            inFile.close();
         } catch (Exception e) {
             DiscoUtils.debugPrintln("File Write Error");
         }
