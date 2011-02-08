fd2dae22cd12fd8a6696e58655e01d0938b34f0a
nchen99@gmail.com <nchen99@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-02-08 19:22:34 +0000

    VelocityMatrices changed to a singleton class

--- FIRST/2011/DiscoMotion/src/Sensors/VelocityMatrices.java
+++ FIRST/2011/DiscoMotion/src/Sensors/VelocityMatrices.java
@@ -3,45 +3,50 @@ package Sensors;
 import Sensors.Jama.*;
 
 /**
- * @author pavchag
+ * @author Nelson Chen
  */
 public class VelocityMatrices {
 
     /*
-     * this should run in its own thread eventually
-     * -nelson
+     * TODO: run in own thread, at least for matrices
      */
 
-    public static final double mass = 54.4310844;   //mass in kg
-    public static final double alpha = 0.5; //weight distribution constanc
-    public static final double thetaFR = 45.0;
-    public static final double thetaFL = 135.0;
-    public static final double thetaBL = 225.0;
-    public static final double thetaBR = 315.0;
-    public static final double radius = 0.635; //radius (wheel-to-wheel) in meters
+    private final double mass = 54.4310844;   //mass in kg
+    private final double alpha = 0.5; //weight distribution constanc
+    private final double thetaFR = 45.0;
+    private final double thetaFL = 135.0;
+    private final double thetaBL = 225.0;
+    private final double thetaBR = 315.0;
+    private final double radius = 0.635; //radius (wheel-to-wheel) in meters
 
-    public static double xVel;
-    static Matrix wheelVelMat   = new Matrix(4, 1);
-    static Matrix wheelForceMat = new Matrix(4, 1);
-    static Matrix velCouplMat   = new Matrix(3, 4);
-    static Matrix forceCouplMat = new Matrix(3, 4);
-    static Matrix accelMatrix   = new Matrix(3, 1);
-    static Matrix velocityMatrix= new Matrix(3, 1);
+    private Matrix wheelVelMat   = new Matrix(4, 1);
+    private Matrix wheelForceMat = new Matrix(4, 1);
+    private Matrix velCouplMat   = new Matrix(3, 4);
+    private Matrix forceCouplMat = new Matrix(3, 4);
+    private Matrix accelMatrix   = new Matrix(3, 1);
+    private Matrix velocityMatrix= new Matrix(3, 1);
+
+    public VelocityMatrices velocityMatrices = new VelocityMatrices();
 
     /*
      *TODO: use encoder values for parameters or take wheel velocities directly
      */
-    public static void setWheelVelMat(double v1, double v2, double v3, double v4) {
+    public void setWheelVelMat(double v1, double v2, double v3, double v4) {
         wheelVelMat.set(0, 0, v1);
         wheelVelMat.set(1, 0, v2);
         wheelVelMat.set(2, 0, v3);
         wheelVelMat.set(3, 0, v4);
     }
 
+    private VelocityMatrices() {
+        initVelCouplMat();
+        //initForceCouplMat();
+    }
+
     /*
      * 
      */
-    public static void calcVelocity() {
+    public void calcVelocity() {
         System.out.println("VELOCITY COUPLING MATRIX, PSEUDOINVERTED");
         
         double[][] raw = velCouplMat.getArray();
@@ -64,39 +69,39 @@ public class VelocityMatrices {
     /*
      *
      */
-    public static void calcAccel() {
+    public void calcAccel() {
         accelMatrix = forceCouplMat.times(wheelForceMat).times(1/mass);
     }
     /*
      * 
      */
-    public static Matrix getAccelMat() {
+    public Matrix getAccelMat() {
         return accelMatrix;
     }
 
     // This code is for the acceleration of the robot.
-    public static void setWheelForceMat(double f1, double f2, double f3, double f4) {
+    public void setWheelForceMat(double f1, double f2, double f3, double f4) {
         wheelForceMat.set(0, 0, f1);
         wheelForceMat.set(1, 0, f2);
         wheelForceMat.set(2, 0, f3);
         wheelForceMat.set(3, 0, f4);
     }
 
-    public static Matrix getVelCouplMat() {
+    public Matrix getVelCouplMat() {
         return velCouplMat;
     }
 
-    public static Matrix getWheelVelMat() {
+    public Matrix getWheelVelMat() {
         return wheelVelMat;
     }
-    public static Matrix getWheelForceMat() {
+    public Matrix getWheelForceMat() {
         return wheelForceMat;
     }
-    public static Matrix getVelocityVector() {
+    public Matrix getVelocityVector() {
         return velocityMatrix;
     }
 
-    public static void initVelCouplMat() {
+    private void initVelCouplMat() {
         double[][] velCoupl = new double[4][3];
         velCoupl[0][0] = -Math.sin(thetaFR);
         velCoupl[0][1] = Math.cos(thetaFR);
@@ -117,7 +122,7 @@ public class VelocityMatrices {
         velCouplMat = velCouplMat.inverse();
     }
 
-    public static void initForceCouplMat() {
+    private void initForceCouplMat() {
         double[][] forceCoupl = new double[3][4];
         forceCoupl[0][0] = -Math.sin(thetaFR);
         forceCoupl[0][1] = -Math.sin(thetaFL);
