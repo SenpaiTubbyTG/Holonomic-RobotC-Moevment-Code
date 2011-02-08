ff9689f21c199099a9fd8cd4c48d415413545ec9
nchen99@gmail.com <nchen99@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-02-08 03:30:52 +0000

    PIDTuner reads PID constants (gain values) from a file, PIDTuning.csv and returns them in a double matrix via getPIDConstants()

---
+++ FIRST/2011/DiscoMotion/src/Utils/PIDTuner.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Utils;
+
+import com.sun.squawk.util.StringTokenizer;
+
+/**
+ * Class PIDTuner
+ *
+ * gets PIDConstants from a file, PIDTuning.csv
+ * File can be of any length as long as it follows this format:
+ *
+ * kP,kI,kD,[doesn't matter after this; label is recommended]
+ * 
+ * example:
+ * 0.13,0.0052,0.021,examplePIDController
+ * 1.2,0.0,0.0,examplePIDController2
+ * 10.5,0.0,5.323,examplePIDController3
+ *
+ *
+ * @author Nelson Chen
+ */
+
+public class PIDTuner {
+
+    /**
+     * returns PID constants read from a file (PIDTuning.csv)
+     * @return double matrix of PID constants in order according to file
+     */
+
+    public static double[][] getPIDConstants() {
+        String[] rawData = FileIO.readFromFile("PIDTuning.csv");
+        double[][] PIDkonstants = new double[rawData.length][3];
+        for (int r = 0; r < PIDkonstants.length; r++) {
+            for (int c = 0; c < 3; c++) {
+                StringTokenizer buff = new StringTokenizer(rawData[r], ",");
+                PIDkonstants[r][c] = Double.parseDouble(buff.nextToken());
+            }
+        }
+        //PIDkonstants matrix should now be initialized with values from the file
+        return PIDkonstants;
+    }
+
+}
