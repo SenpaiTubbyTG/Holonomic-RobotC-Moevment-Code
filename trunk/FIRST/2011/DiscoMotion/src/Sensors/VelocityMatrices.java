fdec3f0912b73f512c3ea61e1c62e44615258ebe
nchen99@gmail.com <nchen99@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-02-08 21:32:41 +0000

    VelocityMatrices now acts as a PIDSource for use in a PIDController object (output can be selected from either x-velocity, y-velocity, or rotational velocity

--- trunk/FIRST/2011/DiscoMotion/src/Sensors/VelocityMatrices.java
+++ trunk/FIRST/2011/DiscoMotion/src/Sensors/VelocityMatrices.java
@@ -1,82 +1,106 @@
 package Sensors;
 
 import Sensors.Jama.*;
+import edu.wpi.first.wpilibj.PIDSource;
+import discobot.HW;
 
 /**
  * @author Nelson Chen
  */
-public class VelocityMatrices {
+public class VelocityMatrices implements PIDSource {
+
+    public static class VelocityOutput {
+        /**
+         * The integer value representing this enumeration
+         */
+        public final int value;
+        static final int kXvelocity_val = 0;
+        static final int kYvelocity_val = 1;
+        static final int kRotationalVelocity_val = 2;
+        /**
+         * Use x-velocity for PIDGet
+         */
+        public static final VelocityOutput kXvelocity = new VelocityOutput(kXvelocity_val);
+        /**
+         * Use y-velocity for PIDGet and PIDWrite
+         */
+        public static final VelocityOutput kYvelocity = new VelocityOutput(kYvelocity_val);
+        /**
+         * Use Twist for PIDGet and PIDWrite
+         */
+        public static final VelocityOutput kRotationalVelocity = new VelocityOutput(kRotationalVelocity_val);
+
+        private VelocityOutput(int value) {
+            this.value = value;
+        }
+    }
 
     /*
      * TODO: run in own thread, at least for matrices
      */
-
-    //private final double mass = 54.4310844;   //mass in kg
-    //private final double alpha = 0.5; //weight distribution constanc
     private final double thetaFR = 45.0;
     private final double thetaFL = 135.0;
     private final double thetaBL = 225.0;
     private final double thetaBR = 315.0;
-    private final double radius = 0.635; //radius (wheel-to-wheel) in meters
 
-    private Matrix wheelVelMat   = new Matrix(4, 1);
+    private VelocityOutput m_output = VelocityOutput.kXvelocity;
+    //private final double radius = 0.635; //radius (wheel-to-wheel) in meters
+    //private final double mass = 54.4310844;   //mass in kg
+    //private final double alpha = 0.5; //weight distribution constant
+
+    private Matrix wheelVelMat    = new Matrix(4, 1);
+    private Matrix velCouplMat    = new Matrix(3, 4);
+    private Matrix velocityMatrix = new Matrix(3, 1);
+
     //private Matrix wheelForceMat = new Matrix(4, 1);
-    private Matrix velCouplMat   = new Matrix(3, 4);
     //private Matrix forceCouplMat = new Matrix(3, 4);
     //private Matrix accelMatrix   = new Matrix(3, 1);
-    private Matrix velocityMatrix= new Matrix(3, 1);
 
+    //Singleton class instance
     public VelocityMatrices velocityMatrices = new VelocityMatrices();
 
-    /*
-     *TODO: use encoder values for parameters or take wheel velocities directly
-     */
-    public void setWheelVelMat(double v1, double v2, double v3, double v4) {
+    private VelocityMatrices() {
+        initVelCouplMat();
+        //initForceCouplMat();
+    }
+    private void setWheelVelocities(double v1, double v2, double v3, double v4) {
         wheelVelMat.set(0, 0, v1);
         wheelVelMat.set(1, 0, v2);
         wheelVelMat.set(2, 0, v3);
         wheelVelMat.set(3, 0, v4);
     }
 
-    private VelocityMatrices() {
-        initVelCouplMat();
-        //initForceCouplMat();
-    }
-
     /*
      * 
      */
     public void calcVelocity() {
-        System.out.println("VELOCITY COUPLING MATRIX, PSEUDOINVERTED");
-        
-        double[][] raw = velCouplMat.getArray();
-        for(int r=0;r<raw.length;r++) {
-            for(int c=0;c<raw[r].length;c++)
-                System.out.print(raw[r][c] + "\t\t\t");
-            System.out.println();
-        }
-
-        System.out.println("\n\nWHEEL VELOCITY MATRIX");
-
-        raw = wheelVelMat.getArray();
-        for(int r=0;r<raw.length;r++) {
-            for(int c=0;c<raw[r].length;c++)
-                System.out.print(raw[r][c]);
-            System.out.println();
-        }
+        setWheelVelocities(HW.encoderFrontLeft.getRate(),
+                           HW.encoderFrontRight.getRate(),
+                           HW.encoderRearRight.getRate(),
+                           HW.encoderRearLeft.getRate());
         velocityMatrix = velCouplMat.times(wheelVelMat);
     }
 
-    public Matrix getVelCouplMat() {
-        return velCouplMat;
+    public double pidGet() {
+        calcVelocity();
+        switch (m_output.value) {
+            case VelocityOutput.kXvelocity_val:
+                return getXvelocity();
+            case VelocityOutput.kYvelocity_val:
+                return getYvelocity();
+            case VelocityOutput.kRotationalVelocity_val:
+                return getRotationalVelocity();
+            default:    //should never be reached
+                return 0.0;
+        }
     }
-    public Matrix getWheelVelMat() {
-        return wheelVelMat;
+    
+    public void setOutput(VelocityOutput output) {
+        m_output = output;
     }
     public Matrix getVelocityVector() {
         return velocityMatrix;
     }
-
     private void initVelCouplMat() {
         double[][] velCoupl = new double[4][3];
         velCoupl[0][0] = -Math.sin(thetaFR);
@@ -96,6 +120,15 @@ public class VelocityMatrices {
         velCoupl[3][2] = 1;
         velCouplMat = new Matrix(velCoupl);
         velCouplMat = velCouplMat.inverse();
+    }
+    public double getXvelocity() {
+        return velocityMatrix.get(0, 0);
+    }
+    public double getYvelocity() {
+        return velocityMatrix.get(1, 0);
+    }
+    public double getRotationalVelocity() {
+        return velocityMatrix.get(2, 0);
     }
 
     /*
