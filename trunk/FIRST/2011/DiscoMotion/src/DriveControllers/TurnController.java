f5ef022286dd555f22427c5e9772803071a18b0e
nchen99@gmail.com <nchen99@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-02-08 23:19:12 +0000

    TurnController can now take joystick input to alter the setpoint (goal heading)

--- trunk/FIRST/2011/DiscoMotion/src/DriveControllers/TurnController.java
+++ trunk/FIRST/2011/DiscoMotion/src/DriveControllers/TurnController.java
@@ -2,6 +2,7 @@ package DriveControllers;
 
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * @author Nelson
@@ -9,7 +10,7 @@ import edu.wpi.first.wpilibj.Gyro;
 public class TurnController {
     private DiscoDriveConverter driveConverter;
     private PIDController gyroController;
-
+    private double incrementStartTime;
     private Gyro gyro;
 
     public TurnController(Gyro g) {
@@ -17,10 +18,19 @@ public class TurnController {
         gyroController = new PIDController(0.1, 0.0, 0.0, gyro, driveConverter);
         driveConverter = 
                 new DiscoDriveConverter(0.0, 0.0, 0.0, DiscoDriveConverter.Output.kTwist);
+        incrementStartTime = Timer.getFPGATimestamp();
     }
 
     public void setSetpoint(double setpt) {
         gyroController.setSetpoint(setpt);
+    }
+
+    public void incrementSetpoint(double inc) {
+        double currentTime = Timer.getFPGATimestamp();
+        double newHeading = gyroController.getSetpoint() +
+                            (inc * (currentTime - incrementStartTime));
+        gyroController.setSetpoint(newHeading);
+        incrementStartTime = currentTime;
     }
 
     public void setPID(double kp, double ki, double kd) {
@@ -29,6 +39,7 @@ public class TurnController {
 
     public void enable() {
         gyroController.enable();
+        incrementStartTime = Timer.getFPGATimestamp();
     }
 
     public void disable() {
