d5834f96b2ef3fb405b35a243d96b10fd38307b5
allen.gregory.iv@gmail.com <allen.gregory.iv@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-02-11 05:37:25 +0000

    LiftController class that I am working on gets rid of the other two files in the class not need with the new way we are going this.

--- FIRST/2011/DiscoMotion/src/DiscoLift/LiftController.java
+++ FIRST/2011/DiscoMotion/src/DiscoLift/LiftController.java
@@ -5,6 +5,9 @@
 package DiscoLift;
 
 import Sensors.DiscoEncoder;
+import discobot.HW;
+import discobot.PWMJaguar;
+import java.util.TimerTask;
 
 /**
  *
@@ -12,16 +15,55 @@ import Sensors.DiscoEncoder;
  */
 public class LiftController {
 
-    private DiscoLift m_discoLift = null;
-    private DiscoEncoder m_encoder = null;
+    public static final double kDefaultPeriod = .01;
+    private PWMJaguar m_jaguar;
+    protected java.util.Timer m_controlLoop;
+    protected double m_period = kDefaultPeriod;
 
-    public LiftController(DiscoLift lift, DiscoEncoder encoder) {
-        m_discoLift = lift;
-        m_encoder = encoder;
+    private double m_defaultLiftSpeedUp = 0.5;
+    private double m_defaultLiftSpeedDown = -0.5;
+
+    private class LiftControllerTask extends TimerTask {
+
+        private LiftController m_controller;
+
+        public LiftControllerTask (LiftController controller) {
+            if (controller == null) {
+                throw new NullPointerException("Given LiftController was null");
+            }
+            m_controller = controller;
+        }
+
+        public void run() {
+            m_controller.checkForLimits();
+        }
     }
 
+    public LiftController() {
+        /*m_jaguar = new PWMJaguar(HW.LiftMotorAddress);
+        m_jaguar.setEncoderChannels(HW.liftEncoderA, HW.liftEncoderB, false);
+        m_jaguar.changeControlMode(PWMJaguar.ControlMode.kPosition);
+        m_jaguar.configEncoderCodesPerRev(HW.liftEncoderCodesPerRev);
 
+         m_controlLoop = new java.util.Timer();
 
+         m_controlLoop.schedule(new LiftControllerTask(this), 0L, (long) (m_period * 1000));
+    */}
 
+    private void checkForLimits(){
+        if (HW.liftLimitInnerDown.get() && HW.liftLimitOuterDown.get()){
+            setPosition(HW.setPositionDown);
+        } else if (HW.liftLimitInnerDown.get() && HW.liftLimitOuterUp.get()) {
+            setPosition(HW.setPositionMiddle);
+        } else if (HW.liftLimitInnerUp.get() && HW.liftLimitOuterDown.get()){
+            setPosition(HW.setPositionMiddle);
+        } else if (HW.liftLimitInnerUp.get() && HW.liftLimitOuterUp.get()){
+            setPosition(HW.setPositionUp);
+        } 
+    }
+
+    private void setPosition(int position){
+
+    }
 
 }
