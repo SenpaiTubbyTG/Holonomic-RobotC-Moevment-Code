18fb1b440d033ab87479d68e1df3f57b22d437f4
pavchag <pavchag@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-02-12 22:08:16 +0000

    Getting robot Velocity, and setting robot Velocity

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ package main;
 
 import Utils.*;
 import edu.wpi.first.wpilibj.*;
+import Jama.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,18 +24,18 @@ public class Main extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-        HW.drive.setInvertedMotor(DiscoDrive.kLeftMotor, true); //Invert Left
-        HW.drive.setInvertedMotor(DiscoDrive.kRightMotor, true); //Invert Right
+        //HW.drive.setInvertedMotor(DiscoDrive.kLeftMotor, true); //Invert Left
+        //HW.drive.setInvertedMotor(DiscoDrive.kRightMotor, true); //Invert Right
     }
 
     /**
      * This function is called once every time the robot is disabled
      */
     public void disabledInit() {
-        HW.frontEncoder.reset();
-        HW.frontEncoder.stop();
-        HW.rearEncoder.reset();
-        HW.rearEncoder.stop();
+        //HW.frontEncoder.reset();
+        //HW.frontEncoder.stop();
+        //HW.rearEncoder.reset();
+        //HW.rearEncoder.stop();
     }
 
     /**
@@ -59,18 +60,60 @@ public class Main extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopInit() {
-        HW.frontEncoder.setDistancePerPulse((8 * Math.PI) / 47 / 3);
-        HW.frontEncoder.start();
-        HW.frontEncoder.reset();
-        HW.rearEncoder.setDistancePerPulse((8 * Math.PI) / 47 / 3);
-        HW.rearEncoder.start();
-        HW.rearEncoder.reset();
+        //HW.frontEncoder.setDistancePerPulse((8 * Math.PI) / 47 / 3);
+        //HW.frontEncoder.start();
+        //HW.frontEncoder.reset();
+        //HW.rearEncoder.setDistancePerPulse((8 * Math.PI) / 47 / 3);
+        //HW.rearEncoder.start();
+        //HW.rearEncoder.reset();
     }
 
     public void teleopPeriodic() {
-        HW.drive.basicDrive(0.0,
+        /*HW.drive.basicDrive(0.0,
                             0.0,
                             HW.driveStick1.getY(),
-                            HW.driveStick2.getY());
+                            HW.driveStick2.getY());*/
+    }
+    public static void runTest()
+    {
+        RobotMotion.initForceCouplMat();
+        RobotMotion.initVelCouplMat();
+        //setWheelVelMat takes
+        //
+        //          (2) +/-    +\- (1)
+        //
+        //          (3) -\+    -/+ (4)
+        //
+        RobotMotion.setWheelVelMat(5, 5, 5, 5);
+
+        Matrix velocity = RobotMotion.getVelocityVector();
+        Matrix acceleration = RobotMotion.getAccelMat();
+
+        //Print Velocity
+        System.out.println("\n\nVELOCITY X, Y, rot");
+        for(int r=0;r<3;r++)
+            System.out.println(velocity.get(r, 0));
+
+        //Print Acceleration
+        /*System.out.println("\n\nACCELERATION X, Y, rot");
+        for(int r=0;r<3;r++)
+            System.out.println(acceleration.get(r, 0));*/
+
+        if (RobotMotion.getSlipMat().times(RobotMotion.getWheelVelMat()).equals(RobotMotion.getZeroMat()))
+        {
+            System.out.println("All is Good!");
+            RobotMotion.setRobVelMat(ROBOT_TASK_PRIORITY, ROBOT_TASK_PRIORITY, ROBOT_TASK_PRIORITY);
+            System.out.println(RobotMotion.getSlipMat().times(RobotMotion.getWheelVelMat()).get(0, 0));
+            System.out.println(RobotMotion.getSlipMat().times(RobotMotion.getWheelVelMat()).get(1, 0));
+            System.out.println(RobotMotion.getSlipMat().times(RobotMotion.getWheelVelMat()).get(2, 0));
+        }
+        else
+        {
+            System.out.println("All is Bad! (Wheels are Slipping)");
+            System.out.println(RobotMotion.getSlipMat().times(RobotMotion.getWheelVelMat()).get(0, 0));
+            System.out.println(RobotMotion.getSlipMat().times(RobotMotion.getWheelVelMat()).get(0, 1));
+            System.out.println(RobotMotion.getSlipMat().times(RobotMotion.getWheelVelMat()).get(0, 2));
+        }
+        
     }
 }
