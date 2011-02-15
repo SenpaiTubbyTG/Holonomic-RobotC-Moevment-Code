29d1c0b4442ad4992bb1017981d10385ee1924fd
nchen99@gmail.com <nchen99@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-02-15 23:01:44 +0000

    reorganized Teleop; removed periodic method from Arm Deleted Collector instance from HW class; updated Arm for new changes (collector motor added to constructor)

--- FIRST/2011/DiscoMotion_Testing/src/discobot/Teleop.java
+++ FIRST/2011/DiscoMotion_Testing/src/discobot/Teleop.java
@@ -8,6 +8,10 @@ import Utils.*;
  */
 public class Teleop {
 
+    public static final int k_collectorInButton = 2;//on lift joystick
+    public static final int k_collectorOutButton = 3;//on lift joystick
+    private static final int k_collectButton = 1; //on left-hand driver joystick
+    private static final int k_armDownButton = 7;//on lift joystick
     static final double k_rotationDeadZone = 0.1;
     static final double k_driveRotationThreshold = 0.5;
     static final double k_gyroRotationThreshold = 1.0;
@@ -15,23 +19,21 @@ public class Teleop {
     static boolean fieldCentricEnabled = true;
     static double currentX = 0.0;
     static double currentY = 0.0;
+    static double oldAngle = 0.0;
+    static double liftSpeed = 0.0;
+    static double rotation;
+    static int i = 0;
     static boolean[] leftButtons = new boolean[12];
     static boolean[] rightButtons = new boolean[12];
     static boolean[] liftButtons = new boolean[12];
-    static int i = 0;
-    static double rotation;
-    static double oldAngle = 0.0;
-    static double liftSpeed = 0.0;
 
-    //static double[] encoder = new double[4];
     public static void periodic() {
-        /*encoder[0] = HW.encoderFrontLeft.getDistance();
-        encoder[1] = HW.encoderFrontRight.getDistance();
-        encoder[2] = HW.encoderRearRight.getDistance();
-        encoder[3] = HW.encoderRearLeft.getDistance();
-        DataLogger.dataLogger.setEntryValue(encoder);*/
         updateButtons();
+        drive();
+        lift();
+    }
 
+    public static void drive() {
         //Turn Controller Reset and Orientation
         if (leftButtons[11]) {
             HW.turnController.reset(0);
@@ -57,8 +59,7 @@ public class Teleop {
             }
         }
 
-        //Drive Code
-        //FC halo control
+        //Field-centric "HALO" control
         rotation = HW.turnController.getRotation();
         double out[] = rotateVector(HW.driveStickLeft.getX(), HW.driveStickLeft.getY(), -1 * HW.gyro.getAngle());
         if (leftButtons[5]) {
@@ -76,6 +77,10 @@ public class Teleop {
         } else {
             HW.drive.HolonomicDrive(out[0], out[1], rotation);
         }
+    }
+
+    public static void lift() {
+        //Lift control
         liftSpeed = -HW.liftHandle.getY();
         if (liftSpeed > 0 && !HW.liftLimitInnerUp.get() && !HW.liftLimitMiddleUp.get()) {
             HW.lift.set(0.0);
@@ -84,19 +89,30 @@ public class Teleop {
         } else {
             HW.lift.set(liftSpeed);
         }
-        HW.arm.periodic(liftButtons);
-        HW.collector.periodic(liftButtons);
+
+        //Arm control for driver and lift operator
+        //arm raises automatically unless ordered downards
+        if (leftButtons[k_collectButton]) {
+            HW.arm.collect();
+        } else if (liftButtons[k_armDownButton]) {
+            HW.arm.down();
+        } else {
+            HW.arm.up();
+        }
+
+        //Manual Collector control by lift operator
+        if (liftButtons[k_collectorInButton]) {
+            HW.arm.tubeIn();
+        } else if (liftButtons[k_collectorOutButton]) {
+            HW.arm.tubeOut();
+        }
     }
 
     public static void continuous() {
         //verifyGyro(HW.gyro.getAngle());
         if (i > 10000) {
-            /*DiscoUtils.debugPrintln("\nLift INNER-UP switch: " + HW.liftLimitInnerUp.get());
-            DiscoUtils.debugPrintln("Lift INNER-DOWN switch: " + HW.liftLimitInnerDown.get());
-            DiscoUtils.debugPrintln("Lift MIDDLE-UP switch: " + HW.liftLimitMiddleUp.get());
-            DiscoUtils.debugPrintln("Lift MIDDLE-DOWN switch: " + HW.liftLimitMiddleDown.get());
-            DiscoUtils.debugPrintln("Arm UP switch:   " + HW.armSwitchUp.get());
-            DiscoUtils.debugPrintln("Arm DOWN switch: " + HW.armSwitchDown.get());*/
+            //DataLogger.dataLogger.setEntryValue(INSERT ARRAY HERE);
+            //DiscoUtils.debugPrintln("\nLift INNER-UP switch: " + HW.liftLimitInnerUp.get());
             i = 0;
         } else {
             i++;
@@ -104,7 +120,7 @@ public class Teleop {
     }
 
     /**
-     * Rotate a vector in Cartesian space.
+     * Rotate a vector tubeIn Cartesian space.
      */
     protected static double[] rotateVector(double x, double y, double angle) {
         double cosA = Math.cos(angle * (3.14159 / 180.0));
