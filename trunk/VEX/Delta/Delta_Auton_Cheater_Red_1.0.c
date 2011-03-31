0cb2e83d4386e3ee63d815eb81c401d617b57454
ndulworth <ndulworth@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-03-31 02:17:02 +0000

    Delta Code so far, code is split into multiple files, one for each auton mode, one for teleop code etc.
    -Nick

--- trunk/VEX/Delta/Delta_Auton_Cheater_Red_1.0.c
+++ trunk/VEX/Delta/Delta_Auton_Cheater_Red_1.0.c
@@ -13,16 +13,22 @@
 #pragma config(Motor,  port9,           ArmLU,         tmotorNormal, openLoop, reversed)
 #pragma config(Motor,  port10,          SuckL,         tmotorNormal, openLoop, reversed)
 
-/////////////////////
-drive_straight(100,5);//speed,inches
+/*******************///should happen at the same time
+drive_straight_unlocked(100,5);//speed,inches
 sucker(127,5);//speed,seconds//suck in red stack
+/*******************/
+
 lock(low_lock_point);//raise arm
 turn(127,90);//speed,degrees turn left
 lock(low_descore_point);//place tubes
 drive_straight(-127,5);// back up
 turn(127,180); //turn to face blue stack
-drive_straight(127,10);//drive to blue stack
+
+/*******************///should happen at the same time
+drive_straight_unlocked(127,10);//drive to blue stack
 sucker(127,5);//suck in blue stack
+/*******************///
+
 turn(127,90);//turn right
 drive_straight(127,7);//drive straight
 turn(127,45);//turn to face tower
