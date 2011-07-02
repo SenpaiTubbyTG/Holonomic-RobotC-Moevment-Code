13bc84e7cdd7910ab1ae90d37c488b80b869e212
nchen99 <nchen99@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-07-02 18:07:05 +0000

    fixed referencing errors workspace builds now

--- DiscoDance/ronelibs/roneos/inc/roneos.h
+++ DiscoDance/ronelibs/roneos/inc/roneos.h
@@ -18,32 +18,8 @@
 #include "../src/System/typedefs.h"
 #include "../src/System/intMath.h"
 #include "../src/System/system.h"
-#include "../src/System/charger.h"
 #include "../src/System/spi.h"
 
-#include "../src/InputOutput/buttons.h"
-#include "../src/InputOutput/leds.h"
-#include "../src/InputOutput/blinky_led.h"
-#include "../src/InputOutput/ir_beacon.h"
-#include "../src/InputOutput/radio.h"
-#include "../src/InputOutput/msp430.h"
-#include "../src/InputOutput/bump_sensor.h"
-#include "../src/InputOutput/sd_card.h"
-
-#include "../src/IRComms/ir_comms.h"
-#include "../src/IRComms/neighbors.h"
-
-#include "../src/Motors/motor.h"
-#include "../src/Motors/encoder.h"
-
-#include "../src/Sensors/gyro.h"
-#include "../src/Sensors/light_sensor.h"
-#include "../src/Sensors/accelerometer.h"
-
-#include "../src/SerialIO/serial.h"
-#include "../src/SerialIO/cfprintf.h"
-#include "../src/System/bl_commands.h"
-
-#include "../src/crc/crctest.h"	//TESTING
+#include "../src/led/leds.h"
 
 #endif /* RONE_H_ */
---
+++ DiscoDance/ronelibs/roneos/src/System/spi.h
@@ -0,0 +1,29 @@
+/*
+ * spi.h
+ *
+ *  Created on: Mar 26, 2011
+ *      Author: jamesm
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#define SPI_NULL                               	0
+#define SPI_LEDS_ONOFF                          3
+#define SPI_LEDS_DIMMER                         4
+
+#define SPI_LEDS_ONOFF_WORDSIZE                 16
+#define SPI_LEDS_DIMMER_WORDSIZE                7
+
+#define LED_LE_SYSCTL                           SYSCTL_PERIPH_GPIOA
+#define LED_LE_PORT                             GPIO_PORTA_BASE
+#define LED_LE_PIN                              GPIO_PIN_6
+
+void SPIInit(void);
+void SPISelectDevice(uint8 device);
+void SPISelectDeviceISR(uint8 device);
+void SPIDeselect(void);
+void SPIDeselectISR(void);
+void SPIConfigureMSP8(void);
+
+#endif /* SPI_H_ */
