f27e859157b0319a41c5784712b8852850778aa5
ndulworth@gmail.com <ndulworth@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-11-11 05:07:32 +0000

    Finished Auton. Simplified main .c file by moving teleop commands to the lib and auton modes to their own file

--- VEX/Zeta/Gateway/zeta_lib 2.c
+++ VEX/Zeta/Gateway/zeta_lib 2.c
@@ -350,11 +350,13 @@ void suck_msec(int speed, int duration) {
 //TELE OP
 
 //SUCK
+
+//6u = up, 6d = down, 8l = rotate left, 8r = rotate right
 void suck() {
-  if (vexRT[Btn5U] == 1) {
+  if (vexRT[Btn6U] == 1) {
     setSuckSpeed(FULL);
   }
-  else if (vexRT[Btn5D] == 1) {
+  else if (vexRT[Btn6D] == 1) {
     setSuckSpeed(-FULL);
   }
   else if (vexRT[Btn8L] == 1) {
@@ -371,26 +373,48 @@ void suck() {
 }
 
 //LIFT
-void lift() {
-  setLiftSpeed((vexRT[Btn5U] - vexRT[Btn5D])*FULL);
-}
 
+/*void lift() {
+  setLiftSpeed((vexRT[Btn5U] - vexRT[Btn5D])*FULL);
+}*/
+
+//5u = up, 6d = down, 7u/7l = left up/up, 7r/7d = right up/down
+void lift() {
+  if (vexRT[Btn5U] == 1) {
+    setLiftSpeed(FULL);
+  }
+  else if (vexRT[Btn5D] == 1) {
+    setLiftSpeed(-FULL);
+  }
+  else if (vexRT[Btn7U] == 1) {
+    setLiftLSpeed(FULL);
+  }
+  else if (vexRT[Btn7L] == 1) {
+    setLiftLSpeed(-FULL);
+  }
+  else if (vexRT[Btn7R] == 1) {
+    setLiftRSpeed(FULL);
+  }
+  else if (vexRT[Btn7D] == 1) {
+    setLiftRSpeed(-FULL);
+  }
+  else {
+    killLift();
+  }
+}
 
 //TANK
 void tankDrive() {
   setDriveLSpeed(vexRT[Ch3]);
   setDriveRSpeed(vexRT[Ch2]);
-
 }
 
- 
- 
-//ARCADE 
- //Arcade functions: Enable this when enabling arcade
-  int joy_x;            // will hold the X value of the analog stick (choices below)
-  int joy_y;            // will hold the Y value of the analog stick (choices below)
-  int threshold = 10;   // helps to eliminate 'noise' from a joystick that isn't perfectly at (0,0) 
- 
+//ARCADE
+//Arcade functions: Enable this when enabling arcade
+int joy_x;            // will hold the X value of the analog stick (choices below)
+int joy_y;            // will hold the Y value of the analog stick (choices below)
+int threshold = 10;   // helps to eliminate 'noise' from a joystick that isn't perfectly at (0,0)
+
 void arcadeDrive() {
   joy_x = vexRT[Ch1];   // This is the RIGHT analog stick.  For LEFT, change 'Ch1' to 'Ch4'.
   joy_y = vexRT[Ch2];   // This is the RIGHT analog stick.  For LEFT, change 'Ch2' to 'Ch3'.
@@ -398,53 +422,56 @@ void arcadeDrive() {
   // Forward, and swing turns: (both abs(X) and abs(Y) are above the threshold, and Y is POSITIVE)
   if((abs(joy_x) > threshold) && (abs(joy_y) > threshold) && (joy_y > 0))
   {
-    motor[leftMotor]  = (joy_y + joy_x)/2;
-    motor[rightMotor] = (joy_y - joy_x)/2;
+    //motor[leftBack] = motor[leftMiddle]= motor[leftFront] = (joy_y + joy_x)/2;
+    setDriveLSpeed((joy_y + joy_x)/2);
+
+    //motor[rightBack] = motor[rightMiddle] = motor[rightFront] = (joy_y - joy_x)/2;
+    setDriveRSpeed((joy_y - joy_x)/2);
   }
   // Backwards and swing turns: (both abs(X) and abs(Y) are above the threshold, and Y is NEGATIVE)
-  else if((abs(joy_x) > threshold) && (abs(joy_y) > threshold) && (joy_y < 0))
-  {
-    motor[leftMotor]  = (joy_y - joy_x)/2;
-    motor[rightMotor] = (joy_y + joy_x)/2;
+  else if((abs(joy_x) > threshold) && (abs(joy_y) > threshold) && (joy_y < 0)) {
+    //motor[leftBack] = motor[leftMiddle]= motor[leftFront] = (joy_y - joy_x)/2;
+    setDriveLSpeed((joy_y - joy_x)/2);
+    //motor[rightBack] = motor[rightMiddle] = motor[rightFront] = (joy_y + joy_x)/2;
+    setDriveRSpeed((joy_y + joy_x)/2);
   }
   // Turning in place: (abs(X) is above the threshold, abs(Y) is below the threshold)
-  else if((abs(joy_x) > threshold) && (abs(joy_y) < threshold))
-  {
-    motor[leftMotor]  = joy_x;
-    motor[rightMotor] = (-1 * joy_x);
+  else if((abs(joy_x) > threshold) && (abs(joy_y) < threshold)) {
+    //motor[leftBack] = motor[leftMiddle]= motor[leftFront] = joy_x;
+    setDriveLSpeed(joy_x);
+    //motor[rightBack] = motor[rightMiddle] = motor[rightFront] = (-1 * joy_x);
+    setDriveRSpeed(-1* joy_x);
   }
   // Standing still: (both abs(X) and abs(Y) are below the threshold)
-  else
-  {
-    motor[leftMotor]  = 0;
-    motor[rightMotor] = 0;
+  else {
+    killDrive();
+  }
+}
+
+////////////////////////Auto Controls////////////////////////////
+
+
+//auto raise suck
+void autoRaiseSuck() {
+  if ((vexRT[Btn8U]) == 1) {
+    if (SensorValue[detectBall] < 200) {
+      setSuckSpeed(FULL);
+    }
+    else {
+      killSuck();
+    }
+  }
+}
+//auto raise lift
+  void autoRaiseLift() {
+    if ((vexRT[Btn7U]) == 1) {
+      lift_btn(FULL, 1);
+    }
   }
 
-}
-
- /////Auto Controls
-
-
-    //auto raise suck
-void autoRaiseSuck() {
-if ((vexRT[Btn8U]) == 1) {
-      if (SensorValue[detectBall] < 200) {
-        setSuckSpeed(FULL);
-      }
-      else {
-        killSuck();
-      }
-      }
-//auto raise lift
-void autoRaiseLift() {
-            if ((vexRT[Btn7U]) == 1) {
-        lift_btn(FULL, 1);
-      }
-      }
-      
-//auto lower lift
-   void autoLowerLift() {  
+  //auto lower lift
+  void autoLowerLift() {
     if ((vexRT[Btn7D]) == 1) {
-        lift_btn(FULL, -1);
-      }
-}
+      lift_btn(FULL, -1);
+    }
+  }
