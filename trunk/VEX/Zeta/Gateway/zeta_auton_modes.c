31f4eafe2944a1e21050ae588d6e9002b75e1cf7
ndulworth@gmail.com <ndulworth@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2011-11-14 17:03:56 +0000

    Post VEX Houston commit of entire vex library. Andy, I have the EeePC. I will return it to you this tuesday

--- trunk/VEX/Zeta/Gateway/zeta_auton_modes.c
+++ trunk/VEX/Zeta/Gateway/zeta_auton_modes.c
@@ -3,7 +3,7 @@
 /*Auton Houston 1*/
 void autonHoustonOne() {
   //deploy collector extension
-  suck_msec(FULL, 500);
+  suck_msec(FULL, 300);
   //wait for match loads & raise lift part of the way
   lift_msec_raw(FULL, 2800); //wait1Msec(2000);
   //drive backwards to low goal
@@ -17,11 +17,11 @@ void autonHoustonOne() {
   //lower lift, load peices for small goal, aim at small goal
   lift_msec_raw(-FULL, 4000);
   //drive to small goal
-  drive_msec(-FULL, 300);
+  drive_msec(-FULL, 3500);
   //score low goal
-  suck_msec(FULL, 2000); //
+  suck_msec(FULL, 5000); //
   //return home
-  drive_msec(-FULL, 300);  //20 seconds
+  drive_msec(FULL, 300);  //20
   //If the rest of this is sped up then take a preload and put it in the floor goal or the  isolation zone
 }
 ////
