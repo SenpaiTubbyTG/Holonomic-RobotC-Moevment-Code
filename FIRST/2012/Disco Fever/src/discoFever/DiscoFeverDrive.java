37e167625ae882cb266af3c35739093db5ab2f8d
martinnikoltchev@gmail.com <martinnikoltchev@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2012-01-11 01:42:47 +0000

    Added constructors to collector and drive train.  The rest of the code will be added tomorrow

--- FIRST/2012/Disco Fever/src/discoFever/DiscoFeverDrive.java
+++ FIRST/2012/Disco Fever/src/discoFever/DiscoFeverDrive.java
@@ -4,10 +4,24 @@
  */
 package discoFever;
 
+import edu.wpi.first.wpilibj.Jaguar;
+
 /**
  *
  * @author mdouglass101
  */
 public class DiscoFeverDrive {
     
+    public static Jaguar m_flDrive;
+    public static Jaguar m_frDrive;
+    public static Jaguar m_blDrive;
+    public static Jaguar m_brDrive;
+    
+    public DiscoFeverDrive(Jaguar m_flDrive, Jaguar m_frDrive, Jaguar m_blDrive, Jaguar m_brDrive){
+        this.m_flDrive = m_flDrive;
+        this.m_frDrive = m_frDrive;
+        this.m_blDrive = m_blDrive;
+        this.m_brDrive = m_brDrive;
+    }
 }
+
