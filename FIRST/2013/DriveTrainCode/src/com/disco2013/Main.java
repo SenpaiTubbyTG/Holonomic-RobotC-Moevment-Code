3a634c122a0c268558d78c5f8595b73affa2b55f
dorishui225@gmail.com <dorishui225@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2013-01-11 01:07:14 +0000

    Compiling drivetrain and shooter code.

--- FIRST/2013/DriveTrainCode/src/com/disco2013/Main.java
+++ FIRST/2013/DriveTrainCode/src/com/disco2013/Main.java
@@ -22,14 +22,7 @@ public class Main extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public static Victor LFM = new Victor(1);
-    public static Victor LBM = new Victor(2);
-    public static Victor RFM = new Victor(3);
-    public static Victor RBM = new Victor(4);
-    public static Victor Shooter1 = new Victor(5);
-    public static RobotDrive drive = new RobotDrive(LFM,LBM,RFM,RBM);
-    public static Joystick Stick1 = new Joystick(1);
-    public static Joystick Stick2 = new Joystick(2);
+    
     public void robotInit() {
     
     }
@@ -45,22 +38,22 @@ public class Main extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        drive.tankDrive(Stick1.getY(),Stick2.getY());
-        if(Stick1.getRawButton(1)==true)
+        HW.drive.tankDrive(HW.Stick1.getY(),HW.Stick2.getY());
+        if(HW.Stick1.getRawButton(1)==true)
         {
-            Shooter1.set(0.25);
+            HW.Shooter1.set(0.25);
         }
-        else if(Stick1.getRawButton(2)==true)
+        else if(HW.Stick1.getRawButton(2)==true)
         {
-            Shooter1.set(0.5);
+            HW.Shooter1.set(0.5);
         }
-        else if(Stick1.getRawButton(3)==true)
+        else if(HW.Stick1.getRawButton(3)==true)
         {
-            Shooter1.set(0.75);
+            HW.Shooter1.set(0.75);
         }
-        else if(Stick1.getRawButton(4)==true)
+        else if(HW.Stick1.getRawButton(4)==true)
         {
-            Shooter1.set(1.0);
+            HW.Shooter1.set(1.0);
         }
         }
     }
