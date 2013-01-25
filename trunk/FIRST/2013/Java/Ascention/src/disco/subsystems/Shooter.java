f01d280da7c7d6075fa55b49bbf6772bb6f74af5
ricktg0@gmail.com <ricktg0@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2013-01-25 03:26:31 +0000

    Started to implement PID for the command based java code

--- trunk/FIRST/2013/Java/Ascention/src/disco/subsystems/Shooter.java
+++ trunk/FIRST/2013/Java/Ascention/src/disco/subsystems/Shooter.java
@@ -6,19 +6,23 @@ package disco.subsystems;
 
 import disco.HW;
 import edu.wpi.first.wpilibj.Victor;
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import disco.utils.DiscoCounterEncoder;
 
 //This is what we have now. This will eventually become a PIDSubsystem (maybe)
-public class Shooter extends Subsystem {
+public class Shooter extends PIDSubsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     private Victor shooter1;
     private Victor shooter2;
+    private static double kP=0, kI=0, kD=0;
+    private DiscoCounterEncoder encoder;
 
     public Shooter(){
-	super("shooter");
+	super("shooter", kP, kI, kD);
 	shooter1=new Victor(HW.Shooter1Slot,HW.Shooter1Channel);
 	shooter2=new Victor(HW.Shooter2Slot,HW.Shooter2Channel);
+        encoder=new DiscoCounterEncoder(HW.DiscoEncoderSlot, HW.DiscoEncoderChannel, 1);
     }
 
     public void initDefaultCommand() {
@@ -45,5 +49,13 @@ public class Shooter extends Subsystem {
 
     public double getPower2(){
 	return shooter2.get();
+    }
+
+    protected double returnPIDInput() {
+        return encoder.getRPM();
+    }
+
+    protected void usePIDOutput(double output) {
+        setPower(output);
     }
 }
