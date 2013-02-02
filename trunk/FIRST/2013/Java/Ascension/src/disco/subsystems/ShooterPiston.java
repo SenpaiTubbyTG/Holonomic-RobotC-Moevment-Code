a066bf62c2bd5a183be6b77b3909152c35bff314
dorishui225@gmail.com <dorishui225@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2013-02-02 20:22:39 +0000

    Added PistonIn and PistonOut commands, added ShooterPiston subsystem.

---
+++ trunk/FIRST/2013/Java/Ascension/src/disco/subsystems/ShooterPiston.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package disco.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Doris
+ */
+public class ShooterPiston extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
