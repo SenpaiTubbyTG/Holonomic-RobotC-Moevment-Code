f747173ee0874ccda70119e913b1d3af7be4fe64
samdietrich@hotmail.com <samdietrich@hotmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2013-02-04 03:40:29 +0000

    Added Secret Formula for joystick sensitivity scaling. Added Bang-Bang control for shooter. Still needs to be mapped to a button.

--- trunk/FIRST/2013/Java/Ascension/src/disco/utils/BetterDrive.java
+++ trunk/FIRST/2013/Java/Ascension/src/disco/utils/BetterDrive.java
@@ -1,76 +1,54 @@
 package disco.utils;
 
-import com.sun.squawk.util.MathUtils;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.communication.UsageReporting;
 
 public class BetterDrive extends RobotDrive {
     public BetterDrive(SpeedController a, SpeedController b,
             SpeedController c, SpeedController d) {
         super(a,b,c,d);
     }
-    
-    double scalar = 2.5;
-    
-    public void tankDrive(double leftValue, double rightValue) {
 
-        // square the inputs (while preserving the sign) to increase fine control while permitting full power
-        leftValue = limit(leftValue);
-        rightValue = limit(rightValue);
-            if (leftValue >= 0.0) {
-                leftValue = (MathUtils.pow(leftValue, scalar));
-            } else {
-                leftValue = -(MathUtils.pow(leftValue, scalar));
-            }
-            if (rightValue >= 0.0) {
-                rightValue = (MathUtils.pow(rightValue, scalar));
-            } else {
-                rightValue = -(MathUtils.pow(rightValue, scalar));
-            }
-        setLeftRightMotorOutputs(leftValue, rightValue);
+    private double Map(double stickVal){
+	double x=10.0*stickVal;
+	//the secret formula
+	return 0.024 + 0.296*x - 0.085*x*x + 0.011*x*x*x - 0.000447*x*x*x*x;
     }
-    public void arcadeDrive(double moveValue, double rotateValue) {
-        // local variables to hold the computed PWM values for the motors
 
-        double leftMotorSpeed;
-        double rightMotorSpeed;
-        
-        
+    public void tankDrive(double leftValue, double rightValue, boolean smoothed) {
+	if(smoothed){
+	    //what? no sign fuction? dumdum.
+	    double leftSign= leftValue>=0 ? 1 : -1;
+	    double rightSign= rightValue>=0 ? 1 : -1;
 
-        moveValue = limit(moveValue);
-        rotateValue = limit(rotateValue);
+	    leftValue=leftSign*Map(Math.abs(leftValue));
+	    rightValue=rightSign*Map(Math.abs(rightValue));
+	}
+	super.tankDrive(leftValue, rightValue, false);
+    }
 
-            // square the inputs (while preserving the sign) to increase fine control while permitting full power
-            if (moveValue >= 0.0) {
-                moveValue =  MathUtils.pow(moveValue, scalar);
-            } else {
-                moveValue = -MathUtils.pow(moveValue, scalar);
-            }
-            if (rotateValue >= 0.0) {
-                rotateValue = MathUtils.pow(rotateValue, scalar);
-            } else {
-                rotateValue = -MathUtils.pow(rotateValue, scalar);
-            }
+    public void tankDrive(double leftValue,double rightValue){
+	super.tankDrive(leftValue, rightValue,false);
+    }
 
-        if (moveValue > 0.0) {
-            if (rotateValue > 0.0) {
-                leftMotorSpeed = moveValue - rotateValue;
-                rightMotorSpeed = Math.max(moveValue, rotateValue);
-            } else {
-                leftMotorSpeed = Math.max(moveValue, -rotateValue);
-                rightMotorSpeed = moveValue + rotateValue;
-            }
-        } else {
-            if (rotateValue > 0.0) {
-                leftMotorSpeed = -Math.max(-moveValue, rotateValue);
-                rightMotorSpeed = moveValue + rotateValue;
-            } else {
-                leftMotorSpeed = moveValue - rotateValue;
-                rightMotorSpeed = -Math.max(-moveValue, -rotateValue);
-            }
-        }
+    public void arcadeDrive(double moveValue, double rotateValue,boolean smoothed) {
+	if(smoothed){
+	    //what? no sign fuction? dumdum.
+	    double moveSign= moveValue>=0 ? 1 : -1;
+	    double rotateSign= rotateValue>=0 ? 1 : -1;
 
-        setLeftRightMotorOutputs(leftMotorSpeed, rightMotorSpeed);
+	    moveValue=moveSign*Map(Math.abs(moveValue));
+	    rotateValue=rotateSign*Map(Math.abs(rotateValue));
+	}
+	super.arcadeDrive(moveValue, rotateValue, false);//no thanks, I'll scale those myself. Reminds me of a story...
+	/*Harold walks into the toilet paper shop, and the guy at the counter says
+	 * "what color toilet paper would you like?"
+	 * Harold says "white please. I'll color it myself."
+	 * Adapted from Captain Underpants
+	 */
+    }
+
+    public void arcadeDrive(double moveValue,double turnValue){
+	super.arcadeDrive(moveValue, moveValue, false);
     }
 }
