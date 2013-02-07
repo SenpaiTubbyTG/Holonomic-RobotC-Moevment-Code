b756a1f4585d495ff54b5ef31fd361f5042bb75c
nshah3524@yahoo.com <nshah3524@yahoo.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2013-02-07 00:13:12 +0000

    Based on expected encoder differences from target angle now.

--- trunk/FIRST/2013/Java/Ascension/src/disco/commands/drivetrain/DriveAngle.java
+++ trunk/FIRST/2013/Java/Ascension/src/disco/commands/drivetrain/DriveAngle.java
@@ -9,93 +9,80 @@ import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
+
 public class DriveAngle extends AssistedTank {
-    final double startPosition = 0;
+    protected PIDController turnControl;
     private double  m_kP=0,
 		    m_kI=0,
 		    m_kD=0;
-    
-    private final double m_correctionThreshold = 0.2;
-    private int m_leftInitial = 0, m_rightInitial = 0;
-    final double angleSetpoint;
-    final double angleStart;
-    double angleChange = 0;
-    
-    private PIDOutput distOutput = new PIDOutput() {
+    protected double m_correction=0;
+    private int m_leftInitial=0;
+    private int m_rightInitial=0;
+    private int m_setpoint = 0;
+
+    private PIDOutput turnOutput = new PIDOutput() {
 
         public void pidWrite(double output) {
             usePIDOutput(output);
         }
     };
-    private PIDSource distSource = new PIDSource() {
- 
+    private PIDSource turnSource = new PIDSource() {
+
         public double pidGet() {
             return returnPIDInput();
         }
     };
-    
 
-    public DriveAngle(double setpoint) {
+    public DriveAngle(double angleSetpoint) {
         requires(drivetrain);
-        this.angleSetpoint = setpoint;
-        angleStart = 0;
-    }
-
-    protected void initialize() {
-        super.initialize();
-
-        m_leftInitial = drivetrain.getRightEncoder();
-        m_rightInitial = drivetrain.getLeftEncoder();
-        
-	turnControl = new PIDController(m_kP, m_kI, m_kD, distSource, distOutput);
-	turnControl.setSetpoint(angleSetpoint);
-        turnControl.enable();
-    }
-    
-    protected void execute() {
-        if(Math.abs(left-right) <= m_correctionThreshold){
-	    if(!turnControl.isEnable()){
-		turnControl.enable();
-	    }
-	    left -= m_correction;
-	    right += m_correction;
-	    double max = Math.max(Math.abs(left), Math.abs(right));
-	    if(max > 1){
-                left = left / max;
-                right = right / max;
-	    }
-	}
-        else {
-	    if(turnControl.isEnable()){
-		turnControl.disable();
-	    }
-	} 
-        drivetrain.tankDrive(left, right);
-        if ((int) angleChange == (int) angleSetpoint) { // Fix degree of error
-            finished = true;
-        }
-    }
-    
-    boolean finished = false;
-    protected boolean isFinished() {
-        return false;
-    }
-    
-    private double returnPIDInput(){
-        /*
-         * TODO: REMEMBER TO SET THESE THREE VARIABLES!
-         */
         double wheelRadius = 0;
         double wheelSeperation = 0;
         int encoderTicks = 0;
-        angleChange = Math.toDegrees((2 * Math.PI) * (wheelRadius / wheelSeperation) * ((offsetRight()) - (offsetLeft()) / encoderTicks));
-        return angleChange;
+ 
+        m_setpoint = (int) (Math.toRadians(angleSetpoint) / (2 * Math.PI) * (wheelSeperation / wheelRadius) * encoderTicks);
     }
+
+    protected void initialize() {
+	super.initialize();
+	m_leftInitial = drivetrain.getLeftEncoder();
+	m_rightInitial = drivetrain.getRightEncoder();
+
+	turnControl = new PIDController(m_kP, m_kI, m_kD, turnSource, turnOutput);
+	turnControl.enable();
+	turnControl.setSetpoint(m_setpoint);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        left -= m_correction;
+	right += m_correction;
+	double max = Math.max(Math.abs(left), Math.abs(right));
+	if(max > 1){
+            left = left / max;
+            right = right / max;
+	}
+	m_leftInitial=drivetrain.getLeftEncoder();
+	m_rightInitial=drivetrain.getRightEncoder();
+        drivetrain.tankDrive(left, right);
+        if (m_correction < 0.2) {
+            finished = true;
+        }
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return finished;
+    }
+    boolean finished = false;
+    private double returnPIDInput(){
+	return offsetLeft()-offsetRight();
+    }
+
     private void usePIDOutput(double output){
 	m_correction=output;
     }
     /*
-     * TODO: TABLES NEED A NAME or they will feel sad. :'(
+     * TODO: TABLES NEED A NAME
      */
     public static double getTargetAngle() {
         return NetworkTable.getTable("").getNumber("mainTargetAngle");
