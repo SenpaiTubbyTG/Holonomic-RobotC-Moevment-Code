a6e8b828ba5e31f873deaf494b8b7b7d329841bb
dorishui225@gmail.com <dorishui225@gmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2013-02-18 02:00:09 +0000

    Added ClearSmall, ClearLarge, SmallClearIn, SmallClearOut, LargeClearIn, LargeClearOut.

---
+++ trunk/FIRST/2013/Java/Ascension/src/disco/commands/pneumatics/ClearLarge.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package disco.commands.pneumatics;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
+
+/**
+ *
+ * @author Doris
+ */
+public class ClearLarge extends CommandGroup {
+    
+    public ClearLarge() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        addSequential(new SmallClearIn());
+        addSequential(new WaitCommand(0.2));
+        addSequential(new SmallClearOut());
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
