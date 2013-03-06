84959c162c6ae95e15804734ca60d1af54714b5c
nshah3524@yahoo.com <nshah3524@yahoo.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2013-03-06 03:36:46 +0000

    Added reset subsystems command. This needs to be implemented into OI!

---
+++ trunk/FIRST/2013/Java/Ascension/src/disco/commands/ResetSubsystems.java
@@ -0,0 +1,43 @@
+package disco.commands;
+
+import disco.utils.Dashboard;
+
+public class ResetSubsystems extends CommandBase {
+    
+    boolean done;
+    
+    public ResetSubsystems() {
+    }
+
+    protected void initialize() {
+        requires(drivetrain);
+        requires(shooter);
+        requires(collector);
+        requires(compressor);
+        requires(solenoid);
+        requires(solenoid); // Prevents any other command from creating problems. 
+        // Make sure all other commmands have a valid interrupted() methods.
+        done = false;
+    }
+
+    protected void execute() {
+        System.out.println("====================================================");
+        System.out.println("Resetting subsystems and OI!");
+        System.out.println("This is potentionally suicidial!");
+        CommandBase.init();
+        Dashboard.init();
+        // This assumes that the robot is NOT multi-threaded! (Except for the Thread sam created)
+        System.out.println("====================================================");
+        done = true;
+    }
+
+    protected boolean isFinished() {
+        return done;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
