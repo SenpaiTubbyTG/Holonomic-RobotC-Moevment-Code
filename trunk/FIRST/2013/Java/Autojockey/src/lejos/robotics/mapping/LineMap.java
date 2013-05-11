4ff6ee8be82d6e7fcdfb410089cd77008db5873b
samdietrich@hotmail.com <samdietrich@hotmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2013-05-11 02:31:06 +0000

    Fixed errors in IO by removing unused (according to netbeans safely delete) files.

--- trunk/FIRST/2013/Java/Autojockey/src/lejos/robotics/mapping/LineMap.java
+++ trunk/FIRST/2013/Java/Autojockey/src/lejos/robotics/mapping/LineMap.java
@@ -15,9 +15,9 @@ import lejos.robotics.navigation.Pose;
 
 /**
  * A map of a room or other closed environment, represented by line segments
- * 
+ *
  * @author Lawrie Griffiths
- * 
+ *
  */
 public class LineMap implements RangeMap, Transmittable {
   private Line[] lines;
@@ -25,7 +25,7 @@ public class LineMap implements RangeMap, Transmittable {
 
   /**
    * Calculate the range of a robot to the nearest wall
-   * 
+   *
    * @param pose the pose of the robot
    * @return the range or -1 if not in range
    */
@@ -49,7 +49,7 @@ public class LineMap implements RangeMap, Transmittable {
 
   /**
    * Create a map from an array of line segments and a bounding rectangle
-   * 
+   *
    * @param lines the line segments
    * @param boundingRect the bounding rectangle
    */
@@ -57,7 +57,7 @@ public class LineMap implements RangeMap, Transmittable {
     this.lines = lines;
     this.boundingRect = boundingRect;
   }
-  
+
   /**
    * Constructor to use when map will be loaded from a data stream
    */
@@ -66,7 +66,7 @@ public class LineMap implements RangeMap, Transmittable {
 
   /**
    * Check if a point is within the mapped area
-   * 
+   *
    * @param p the Point
    * @return true iff the point is with the mapped area
    */
@@ -89,13 +89,13 @@ public class LineMap implements RangeMap, Transmittable {
 
   /**
    * Return the bounding rectangle of the mapped area
-   * 
+   *
    * @return the bounding rectangle
    */
   public Rectangle getBoundingRect() {
     return boundingRect;
   }
-  
+
   /**
    * Dump the map to a DataOutputStream
    * @param dos the stream
@@ -109,7 +109,7 @@ public class LineMap implements RangeMap, Transmittable {
         dos.writeFloat(lines[i].x2);
         dos.writeFloat(lines[i].y2);
         dos.flush();
-      }  
+      }
       dos.writeFloat(boundingRect.x);
       dos.writeFloat(boundingRect.y);
       dos.writeFloat(boundingRect.width);
@@ -118,7 +118,7 @@ public class LineMap implements RangeMap, Transmittable {
   }
   /**
    * Load a map from a DataInputStream
-   * 
+   *
    * @param dis the stream
    * @throws IOException
    */
@@ -126,58 +126,44 @@ public class LineMap implements RangeMap, Transmittable {
       lines = new Line[dis.readInt()];
       for(int i=0;i<lines.length;i++) {
         float x1 = dis.readFloat();
-        float y1 = dis.readFloat(); 
+        float y1 = dis.readFloat();
         float x2 = dis.readFloat();
         float y2 = dis.readFloat();
         lines[i] = new Line(x1,y1,x2,y2);
-      }     
+      }
       boundingRect = new Rectangle(dis.readFloat(),dis.readFloat(),dis.readFloat(),dis.readFloat());
   }
-  
+
   /**
    * Get the lines as an array
-   * 
+   *
    * @return the lines as an array
    */
   public Line[] getLines() {
 	  return lines;
   }
-  
+
   /**
    * Create an SVG map file
-   * 
+   *
    * @param fileName the name of the file to create or overwrite
    * @throws IOException
    */
-  public void createSVGFile(String fileName) throws IOException {
-    File mapFile = new File(fileName);
-    FileOutputStream fos = new FileOutputStream(mapFile);
-    PrintStream ps = new PrintStream(fos);
-    ps.println("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" + boundingRect.width + "px\" height=\""
-            + boundingRect.height + "px\" viewBox=\""+boundingRect.x+" "+boundingRect.y+" "+boundingRect.width+" "+boundingRect.height+"\">");
-    ps.println("<g>");
-    for(int i=0;i<lines.length;i++) {
-      ps.println("<line stroke=\"#000000\" x1=\"" + lines[i].x1 + "\" y1=\"" + lines[i].y1 + "\" x2=\"" + lines[i].x2 + "\" y2=\"" + lines[i].y2 + "\"/>");
-    }
-    ps.println("</g>");
-    ps.println("</svg>");
-    ps.close();
-    fos.close();
-  }
-  
+//DELETED
+
   /**
    * Create a line map with the y axis flipped
-   *  
+   *
    * @return the new LineMap
    */
   public LineMap flip() {
 	  float maxY = boundingRect.y + boundingRect.height;
 	  Line[] ll = new Line[lines.length];
-	  
+
 	  for(int i=0;i<lines.length;i++) {
 		  ll[i] = new Line(lines[i].x1, maxY - lines[i].y1, lines[i].x2, maxY - lines[i].y2);
 	  }
-	  
+
 	  return new LineMap(ll, boundingRect);
   }
 }
