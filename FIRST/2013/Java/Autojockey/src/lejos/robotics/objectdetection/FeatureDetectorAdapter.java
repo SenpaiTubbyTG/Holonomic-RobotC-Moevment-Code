d326d63f7ee04392cc158dfe056dd8b5887de3d2
samdietrich@hotmail.com <samdietrich@hotmail.com@db174629-2cb1-8c4e-3c38-f4a78f916a08>
2013-05-11 01:50:28 +0000

    Fixed generics errors in feature detection

--- FIRST/2013/Java/Autojockey/src/lejos/robotics/objectdetection/FeatureDetectorAdapter.java
+++ FIRST/2013/Java/Autojockey/src/lejos/robotics/objectdetection/FeatureDetectorAdapter.java
@@ -1,6 +1,7 @@
 package lejos.robotics.objectdetection;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /*
  * WARNING: THIS CLASS IS SHARED BETWEEN THE classes AND pccomms PROJECTS.
@@ -8,32 +9,32 @@ import java.util.ArrayList;
  */
 
 /**
- * An adapter to make it easier to implement FeatureDetector classes. The scan() method is the only method 
+ * An adapter to make it easier to implement FeatureDetector classes. The scan() method is the only method
  * which must be implemented by the actual class.
- *  
+ *
  * @author BB
  *
  */
 public abstract class FeatureDetectorAdapter implements FeatureDetector {
 
-	private ArrayList<FeatureListener> listeners = null;	
+	private ArrayList listeners = null;
 	private boolean enabled = true;
-	private int delay = 0; 
-	
+	private int delay = 0;
+
 	public FeatureDetectorAdapter(int delay) {
 		this.delay = delay;
 		Thread x = new MonitorThread();
 		x.setDaemon(true);
 		x.start();
 	}
-	
+
 	public void addListener(FeatureListener l){
-		if(listeners == null )listeners = new ArrayList<FeatureListener>();
+		if(listeners == null )listeners = new ArrayList();
 		listeners.add(l);
 	}
 
 	public void enableDetection(boolean enable) {
-		// TODO: Optionally do a real disable where it ends thread (true test in thread loop) and 
+		// TODO: Optionally do a real disable where it ends thread (true test in thread loop) and
 		// enabling it will start thread (if thread is null/not running).
 		this.enabled = enable;
 	}
@@ -45,11 +46,11 @@ public abstract class FeatureDetectorAdapter implements FeatureDetector {
 	public int getDelay() {
 		return delay;
 	}
-	
+
 	public void setDelay(int delay) {
 		this.delay = delay;
 	}
-	
+
 	/**
 	 * Thread to monitor the range finder.
 	 *
@@ -57,21 +58,21 @@ public abstract class FeatureDetectorAdapter implements FeatureDetector {
 	private class MonitorThread extends Thread{
 
 		long prev_time;
-		
-		
+
+
 		public void run() {
 			while(true) {
 				// Only performs scan if detection is enabled.
 				Feature f = (enabled?scan():null);
 				if(f != null) notifyListeners(f);
-				
+
 				try {
 					long elapsed_time = System.currentTimeMillis() - prev_time;
-					
-					
+
+
 					long actual_delay = delay - elapsed_time;
 					if(actual_delay < 0) actual_delay = 0;
-					
+
 					Thread.sleep(actual_delay);
 					prev_time = System.currentTimeMillis();
 				} catch (InterruptedException e) {
@@ -82,13 +83,14 @@ public abstract class FeatureDetectorAdapter implements FeatureDetector {
 	}
 
 	protected void notifyListeners(Feature feature) {
-		if(listeners != null) { 
-			for(FeatureListener l : listeners) {
-				l.featureDetected(feature, this);
-			}
+		if(listeners != null) {
+		    for (Iterator it = listeners.iterator(); it.hasNext();) {
+			FeatureListener l = (FeatureListener) it.next();
+			l.featureDetected(feature, this);
+		    }
 		}
 	}
-	
+
 	public abstract Feature scan();
 
 }
